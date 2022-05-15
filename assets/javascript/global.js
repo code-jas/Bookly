9849af3a0c43bdcde0eae73fe573494f2b65c770
Angelo Silvestre <johnangelo.silvestre04@gmail.com>
2022-05-15 08:58:36 +0800

    installation home page

---
+++ assets/javascript/global.js
@@ -0,0 +1,38 @@
+/****************  hamburger (mobile view) - start     *********************/
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-link");
+
+clickCount = 0;
+hamburger.addEventListener("click", () => {
+   hamburger.classList.toggle("navbar-active");
+   navMenu.classList.toggle("navbar-active");
+   ++clickCount;
+   console.log(clickCount);
+   if (clickCount % 2 === 0) {
+      document.querySelector("body").style.overflow = "auto";
+   
+
+   } else {
+
+      document.querySelector("body").style.overflow = "hidden";
+   }
+})
+
+navLinks.forEach(n => n.addEventListener("click", () => {
+   hamburger.classList.remove("navbar-active");
+   navMenu.classList.toggle("navbar-active");
+   document.querySelector("body").style.overflow = "auto";
+}))
+
+/****************  hamburger (mobile view) - end     *********************/
+
+
+/****************  change bg navbar when scroll - start     *********************/
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () =>
+   header.classList.toggle("sticky", window.scrollY > 0)
+)
+/****************  change bg navbar when scroll - start     *********************/
