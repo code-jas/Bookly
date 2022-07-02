141687227bb52274050379201d377851e7e04b53
Angelo Silvestre <johnangelo.silvestre04@gmail.com>
2022-07-02 22:08:32 +0800

    db update

--- assets/db/bookly_db.sql
+++ assets/db/bookly_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 23, 2022 at 06:59 PM
+-- Generation Time: Jul 02, 2022 at 04:07 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.1
 
@@ -45,7 +45,9 @@ CREATE TABLE `account_sign_up_verification` (
 INSERT INTO `account_sign_up_verification` (`id`, `username`, `password`, `confirm_password`, `email`, `vkey`, `verified`, `createdate`) VALUES
 (42, 'JohnAngelo04', 'JohnName@102', '', 'johnangelo.silvestre@tup.edu.ph', '0f3f176db2c50291180b340c971a5116', 1, '2022-06-19 13:24:58.627346'),
 (52, 'RyanTecling', 'P@werShell10', '', 'kiveh45578@syswift.com', '7a743b1f15a5d5cd33fd6c4f02ae59db', 0, '2022-06-19 14:58:03.674676'),
-(53, 'tagryze04', 'jOhnname@102', '', 'mawebal998@tagbert.com', '972090992fad617d6725ccc518c568ac', 1, '2022-06-22 14:09:08.291999');
+(53, 'tagryze04', 'jOhnname@102', '', 'mawebal998@tagbert.com', '972090992fad617d6725ccc518c568ac', 1, '2022-06-22 14:09:08.291999'),
+(54, 'Tag-ryze04', 'maRinduQQu3$', '', 'natewig164@tagbert.com', '6febbd7872e628edd0d1479c2a0c98e1', 1, '2022-06-26 17:26:12.057103'),
+(56, 'JhonTaba123', 'Jhon123@123', '', 'johnangelo.silvestre04@gmail.com', '21b0f97437e76b01730533872c1238de', 1, '2022-07-02 13:19:02.318109');
 
 -- --------------------------------------------------------
 
@@ -75,8 +77,8 @@ CREATE TABLE `account_user` (
 --
 
 INSERT INTO `account_user` (`id_user`, `firstname`, `lastname`, `contact`, `birthdate`, `username`, `email`, `password`, `address`, `barangay`, `city`, `zipcode`, `profile_img`, `account_type`) VALUES
-(6, 'John Angelo', 'Silvestre', '09562141708', '2000-01-03', 'JohnAngelo04', 'johnangelo.silvestre@tup.edu.ph', 'maRinduQQu3$', '1230 Tambunting st sta cruz', '374', 'Manila', '1010', './assets/images/profile_imgs/5833_STC-1.jpg', '2'),
-(8, 'Angelo John', 'Banting', '', '0000-00-00', 'tagryze04', 'mawebal998@tagbert.com', 'jOhnname@102', '1230 Tambunting st sta cruz', '374', 'Manila', '1014', './assets/images/profile_imgs/STC-1.jpg', '2');
+(6, 'John Angelo', 'Silvestre', '09562141708', '2000-01-03', 'JohnAngelo04', 'johnangelo.silvestre@tup.edu.ph', 'JhonTaba!123', '1230 Tambunting st sta cruz', '374', 'Manila', '1010', './assets/images/profile_imgs/poster_tanjiro.jpg', '2'),
+(11, 'Jhon Java', 'Yu', '09292243005', '2016-07-03', 'JhonTaba123', 'johnangelo.silvestre04@gmail.com', 'KimValorant123#', '300 Bahay nila kim', '121', 'NavotasCity', '1314', './assets/images/profile_imgs/cover_zenitsu.jpg', '2');
 
 --
 -- Indexes for dumped tables
@@ -102,13 +104,13 @@ ALTER TABLE `account_user`
 -- AUTO_INCREMENT for table `account_sign_up_verification`
 --
 ALTER TABLE `account_sign_up_verification`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `account_user`
 --
 ALTER TABLE `account_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
